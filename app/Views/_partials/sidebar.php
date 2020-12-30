2766526aa0afded7273b35cce01f923f10a8e94d
ardyanfalah <ardyanfalah@gmail.com>
2020-12-30 20:00:16 +0700

    ganti nama

--- app/Views/_partials/sidebar.php
+++ app/Views/_partials/sidebar.php
@@ -2,7 +2,7 @@
     <a href="<?php echo base_url('/'); ?>" class="brand-link">
       <img src="<?php echo base_url('themes/dist'); ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">SIP Online</span>
+      <span class="brand-text font-weight-light">Akaci</span>
     </a>
 
     <div class="sidebar">
